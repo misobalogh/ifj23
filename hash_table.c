e0c813d595d8fc94c8942243a2d5952b6397b36b
Michal Balogh <baloghmichal03@gmail.com>
2023-10-04 15:16:56 +0200

    test2

---
+++ hash_table.c
@@ -0,0 +1,11 @@
+/***************************************************************
+* Project Name: Implementace překladače imperativního jazyka IFJ23
+* File Name: 
+* Description: 
+* Author: 
+* Faculty: FIT VUT
+* Date: 
+
+* Comments:
+
+***************************************************************/
