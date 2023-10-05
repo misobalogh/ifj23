1a9039bb050706e54cc4bab16adc0a0f7b891e53
Michal Balogh <baloghmichal03@gmail.com>
2023-10-05 16:06:03 +0200

    testovaci commit

---
+++ test.h
@@ -0,0 +1,12 @@
+/***************************************************************
+* Project Name: Implementace překladače imperativního jazyka IFJ23
+* File Name: 
+* Description: 
+* Author: 
+* Faculty: FIT VUT
+* Date: 
+
+* Comments:
+
+***************************************************************/
+
