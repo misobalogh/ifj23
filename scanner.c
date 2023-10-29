85d584db8133917afe7864565ce285f7f1a25458
Tadeáš Zobal <tadeaszobal@seznam.cz>
2023-10-29 16:12:14 +0100

    first part of FSM done, should be able to process symbols

--- scanner.c
+++ scanner.c
@@ -1,20 +1,52 @@
 #include <stdio.h>
 #include "scanner.h"
 #include <ctype.h>
+#include <stdlib.h>
 
-states current_lex_state = STATE_START;
+// function initializes dynamic string for reading nubers,string and identifiers
+void string_init(dynamic_string *str)
+{
+    str->size = 0;      // initial current size
+    str->capacity = 32; // initial capacity of string
+    str->data = (char *)malloc(str->capacity * sizeof(char));
+
+    if (str->data == NULL)
+    {
+        printf("Malloc of dynamic string failed\n");
+        return;
+    }
+}
+
+// if dynamic string is full, function will realloc its memory to 2 times its previous size
+void char_insert(dynamic_string *str, char c)
+{
+    if (str->size == str->capacity)
+    {
+        str->capacity = str->capacity * 2; // doubles the maximum capacity
+        str->data = (char *)realloc(str->data, str->capacity);
+        if (str->data == NULL)
+        {
+            printf("Realloc of dynamic string failed\n");
+            return;
+        }
+    }
+}
+
 lex_token current_lex_token = {.token_type = TYPE_ERROR, .token_value = 0};
 
 lex_token get_next_token()
 {
-    char c;
-    // comments and block comments
-    while ((c = getc(stdin)) != EOF)
+    states current_lex_state = STATE_START;
+
+    int c = 0;
+    while (1)
     {
+        c = getc(stdin);
         switch (current_lex_state)
         {
         case STATE_START:
-            if (isspace(c))
+            // if (isspace(c))
+            if (c == ' ')
             {
                 continue;
             }
@@ -26,7 +58,54 @@ lex_token get_next_token()
             {
                 current_lex_state = STATE_GREATER_THAN;
             }
-
+            else if (c == '<')
+            {
+                current_lex_state = STATE_LESS_THAN;
+            }
+            else if (c == EOF)
+            {
+                current_lex_state = STATE_EOF;
+            }
+            else if (c == '+')
+            {
+                current_lex_state = STATE_ADDITION;
+            }
+            else if (c == '-')
+            {
+                current_lex_state = STATE_SUBTRACTION;
+            }
+            else if (c == '*')
+            {
+                current_lex_state = STATE_MULTIPLICATION;
+            }
+            else if (c == '?')
+            {
+                current_lex_state = STATE_OPTIONAL;
+            }
+            else if (c == ';')
+            {
+                current_lex_state = STATE_SEMICOLON;
+            }
+            else if (c == '(')
+            {
+                current_lex_state = STATE_LEFT_BRACKET;
+            }
+            else if (c == ')')
+            {
+                current_lex_state = STATE_RIGHT_BRACKET;
+            }
+            else if (c == '\n')
+            {
+                current_lex_state = STATE_EOL;
+            }
+            else if (c == '!')
+            {
+                current_lex_state = STATE_NOT;
+            }
+            else if (c == '=')
+            {
+                current_lex_state = STATE_ASSING;
+            }
             else
             {
                 fprintf(stderr, "ERROR: unexpected character encountered: %c\n", c);
@@ -43,6 +122,10 @@ lex_token get_next_token()
             else if (c == '*')
             {
                 current_lex_state = STATE_BLOCKCOMMENT;
+            }
+            else if (isspace(c))
+            {
+                current_lex_state = STATE_DIVISION;
             }
             else
             {
@@ -86,14 +169,163 @@ lex_token get_next_token()
         case STATE_GREATER_THAN:
             if (c == '=')
             {
+                // moves to STATE_GREATER_OR_EQUAL_THEN
                 current_lex_state = STATE_GREATER_OR_EQUAL_THAN;
-                current_lex_token.token_type = TYPE_OP_GREATER_THAN;
+                // current_lex_state = STATE_START;
             }
             else
             {
-                current_lex_token.token_type = TYPE_OP_GREATER_OR_EQUAL_THAN;
+                current_lex_token.token_type = TYPE_OP_GREATER_THAN;
+                // current_lex_state = STATE_START;
                 ungetc(c, stdin);
+                return current_lex_token;
             }
+
+            break;
+
+        case STATE_GREATER_OR_EQUAL_THAN:
+            current_lex_token.token_type = TYPE_OP_GREATER_OR_EQUAL_THAN;
+            return current_lex_token;
+
+            break;
+
+        case STATE_LESS_THAN:
+            if (c == '=')
+            {
+                current_lex_state = STATE_LESS_OR_EQUAL_THAN;
+            }
+            else
+            {
+                current_lex_token.token_type = TYPE_OP_LESS_THAN;
+                ungetc(c, stdin);
+                return current_lex_token;
+            }
+            break;
+
+        case STATE_LESS_OR_EQUAL_THAN:
+            current_lex_token.token_type = TYPE_OP_LESS_OR_EQUAL_THAN;
+            ungetc(c, stdin);
+            return current_lex_token;
+
+            break;
+
+        case STATE_ADDITION:
+            current_lex_token.token_type = TYPE_ADD;
+            ungetc(c, stdin);
+            return current_lex_token;
+
+            break;
+
+        case STATE_SUBTRACTION:
+            current_lex_token.token_type = TYPE_SUB;
+            ungetc(c, stdin);
+            return current_lex_token;
+
+            break;
+
+        case STATE_MULTIPLICATION:
+            current_lex_token.token_type = TYPE_MUL;
+            ungetc(c, stdin);
+            return current_lex_token;
+
+            break;
+
+        case STATE_DIVISION:
+            current_lex_token.token_type = TYPE_DIV;
+            ungetc(c, stdin);
+            return current_lex_token;
+
+            break;
+
+        case STATE_OPTIONAL:
+            if (c == '?')
+            {
+                current_lex_state = STATE_DEFAULT_VALUE;
+            }
+            else
+            {
+                current_lex_token.token_type = TYPE_OPTIONAL;
+                ungetc(c, stdin);
+                return current_lex_token;
+            }
+
+            break;
+
+        case STATE_DEFAULT_VALUE:
+            current_lex_token.token_type = TYPE_DEFAULT_VALUE;
+            ungetc(c, stdin);
+            return current_lex_token;
+
+            break;
+
+        case STATE_EOF:
+
+            current_lex_token.token_type = TYPE_EOF;
+            ungetc(c, stdin);
+            return current_lex_token;
+
+            break;
+
+        case STATE_SEMICOLON:
+            current_lex_token.token_type = TYPE_SEMICOLON;
+            ungetc(c, stdin);
+            return current_lex_token;
+
+            break;
+        case STATE_LEFT_BRACKET:
+            current_lex_token.token_type = TYPE_LEFT_BRACKET;
+            ungetc(c, stdin);
+            return current_lex_token;
+
+            break;
+
+        case STATE_RIGHT_BRACKET:
+            current_lex_token.token_type = TYPE_RIGHT_BRACKET;
+            ungetc(c, stdin);
+            return current_lex_token;
+
+            break;
+
+        case STATE_EOL:
+            current_lex_token.token_type = TYPE_EOL;
+            ungetc(c, stdin);
+            return current_lex_token;
+            break;
+
+        case STATE_NOT:
+            if (c == '=')
+            {
+                current_lex_state = STATE_NOT_EQUAL;
+            }
+            else
+            {
+                current_lex_token.token_type = TYPE_NOT;
+                ungetc(c, stdin);
+                return current_lex_token;
+            }
+            break;
+
+        case STATE_NOT_EQUAL:
+            current_lex_token.token_type = TYPE_NOT_EQUAL;
+            return current_lex_token;
+            break;
+
+        case STATE_ASSING:
+            if (c == '=')
+            {
+                current_lex_state = STATE_EQUAL;
+            }
+            else
+            {
+                current_lex_token.token_type = TYPE_ASSING;
+                ungetc(c, stdin);
+                return current_lex_token;
+            }
+            break;
+
+        case STATE_EQUAL:
+            current_lex_token.token_type = TYPE_EQUAL;
+            ungetc(c, stdin);
             return current_lex_token;
 
             break;
--- scanner.h
+++ scanner.h
@@ -27,6 +27,7 @@ typedef enum States
     STATE_LEFT_BRACKET,
     STATE_RIGHT_BRACKET,
     STATE_EQUAL,
+    STATE_ASSING,
     STATE_NOT_EQUAL,
     STATE_LESS_THAN,
     STATE_LESS_OR_EQUAL_THAN,
@@ -36,23 +37,68 @@ typedef enum States
     STATE_SUBTRACTION,
     STATE_MULTIPLICATION,
     STATE_DIVISION,
-    STATE_NUMBER,
-    STATE_STRING, // to do string
     STATE_INT,
-    STATE_DOUBLE,  // to do
-    STATE_KEYWORD, // to do
-} states;
+    STATE_EXP_DECIMAL,
+    STATE_EXP,          // e
+    STATE_EXP_POWER,    //^10
+    STATE_EXP_OPERATOR, //+-
+    STATE_DEC_NUMBER,   // .(decimal number)
+    STATE_SEMICOLON,
+    STATE_OPTIONAL,      //?
+    STATE_DEFAULT_VALUE, //??
+    STATE_UNDERSCORE_IDENTIFIER,
+    STATE_IDENTIFIER,
+    STATE_NOT,
+    STATE_EOF,
+    STATE_EOL,
+    STATE_I_Int,
+    STATE_N_Int,
+    STATE_Int,
+    STATE_Int ?,
+    STATE_D_Double,
+    STATE_O_Double,
+    STATE_U_Double,
+    STATE_B_Double,
+    STATE_L_Double,
+    STATE_Double,
+    STATE_DOUBLE ?,
+    STATE_S_Sring,
+    STATE_T_Sring,
+    STATE_R_Sring,
+    STATE_I_Sring,
+    STATE_N_Sring,
+    STATE_Sring,
+    STATE_Sring ?, } states;
 
 typedef enum Type_of_token
 {
     TYPE_ERROR,
     TYPE_EOF,
+    TYPE_EOL,
     TYPE_ADD,
     TYPE_SUB,
     TYPE_DIV,
     TYPE_MUL,
     TYPE_OP_GREATER_THAN,
     TYPE_OP_GREATER_OR_EQUAL_THAN,
+    TYPE_OP_LESS_THAN,
+    TYPE_OP_LESS_OR_EQUAL_THAN,
+    TYPE_OPTIONAL,
+    TYPE_DEFAULT_VALUE,
+    TYPE_SEMICOLON,
+    TYPE_LEFT_BRACKET,
+    TYPE_RIGHT_BRACKET,
+    TYPE_WHOLE_NUMBER,
+    TYPE_EXP_DECIMAL,
+    TYPE_DEC_NUMBER,
+    TYPE_EXP,
+    TYPE_EXP_OPERATOR,
+    TYPE_EXP_POWER,
+    TYPE_NOT,
+    TYPE_NOT_EQUAL,
+    TYPE_EQUAL,
+    TYPE_ASSING,
+    TYPE_IDENTIFIER,
 } type_of_token;
 
 typedef union Value_of_token
@@ -61,6 +107,13 @@ typedef union Value_of_token
     float FLOAT_VAL;
     char *STR_VAL;
 } value_of_token;
+
+typedef struct Dynamic_String
+{
+    char *data;            // current cuntent
+    unsigned int size;     // current size
+    unsigned int capacity; // max capacity
+} dynamic_string;
 
 typedef struct Token
 {
