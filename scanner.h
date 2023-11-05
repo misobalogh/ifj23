a507ec3eba3cbe87015e940c4574f43899943624
Tadeáš Zobal <tadeaszobal@seznam.cz>
2023-11-05 10:03:20 +0100

    lexer almost finished, known issues are strings, nested commnent and underscore token

--- scanner.h
+++ scanner.h
@@ -1,21 +1,3 @@
-
-
-// enum of IFJ23 keywords
-typedef enum Keyword
-{
-    KEYWORD_DOUBLE,
-    KEYWORD_ELSE,
-    KEYWORD_FUNC,
-    KEYWORD_IF,
-    KEYWORD_INT,
-    KEYWORD_LET,
-    KEYWORD_NIL,
-    KEYWORD_RETURN,
-    KEYWORD_STRING,
-    KEYWORD_VAR,
-    KEYWORD_WHILE,
-} keyword;
-
 // enum of FSM states
 typedef enum States
 {
@@ -26,6 +8,10 @@ typedef enum States
     STATE_BLOCKCOMMENT_STAR,
     STATE_LEFT_BRACKET,
     STATE_RIGHT_BRACKET,
+    STATE_LEFT_CURV_BRACKET,
+    STATE_RIGHT_CURV_BRACKET,
+    STATE_LEFT_SQUARE_BRACKET,
+    STATE_RIGHT_SQUARE_BRACKET,
     STATE_EQUAL,
     STATE_ASSING,
     STATE_NOT_EQUAL,
@@ -37,7 +23,7 @@ typedef enum States
     STATE_SUBTRACTION,
     STATE_MULTIPLICATION,
     STATE_DIVISION,
-    STATE_INT,
+    STATE_WHOLE_NUMBER,
     STATE_EXP_DECIMAL,
     STATE_EXP,          // e
     STATE_EXP_POWER,    //^10
@@ -51,24 +37,28 @@ typedef enum States
     STATE_NOT,
     STATE_EOF,
     STATE_EOL,
+    STATE_RIGHT_ARROW,
+    STATE_LEFT_ARROW,
     STATE_I_Int,
     STATE_N_Int,
     STATE_Int,
-    STATE_Int ?,
+    STATE_Int_Q,
     STATE_D_Double,
     STATE_O_Double,
     STATE_U_Double,
     STATE_B_Double,
     STATE_L_Double,
     STATE_Double,
-    STATE_DOUBLE ?,
-    STATE_S_Sring,
-    STATE_T_Sring,
-    STATE_R_Sring,
-    STATE_I_Sring,
-    STATE_N_Sring,
-    STATE_Sring,
-    STATE_Sring ?, } states;
+    STATE_DOUBLE_Q,
+    STATE_S_String,
+    STATE_T_String,
+    STATE_R_String,
+    STATE_I_String,
+    STATE_N_String,
+    STATE_String,
+    STATE_String_Q,
+    STATE_COLON,
+} states;
 
 typedef enum Type_of_token
 {
@@ -88,17 +78,41 @@ typedef enum Type_of_token
     TYPE_SEMICOLON,
     TYPE_LEFT_BRACKET,
     TYPE_RIGHT_BRACKET,
+    TYPE_LEFT_CURV_BRACKET,
+    TYPE_RIGHT_CURV_BRACKET,
+    TYPE_LEFT_SQUARE_BRACKET,
+    TYPE_RIGHT_SQUARE_BRACKET,
+    TYPE_RIGHT_ARROW,
+    TYPE_LEFT_ARROW,
     TYPE_WHOLE_NUMBER,
-    TYPE_EXP_DECIMAL,
     TYPE_DEC_NUMBER,
-    TYPE_EXP,
-    TYPE_EXP_OPERATOR,
     TYPE_EXP_POWER,
     TYPE_NOT,
     TYPE_NOT_EQUAL,
     TYPE_EQUAL,
     TYPE_ASSING,
     TYPE_IDENTIFIER,
+    TYPE_COLON,
+    TYPE_INT,
+    TYPE_INT_Q,
+    TYPE_DOUBLE,
+    TYPE_DOUBLE_Q,
+    TYPE_STRING,
+    TYPE_STRING_Q,
+    KEYWORD_DOUBLE,
+    KEYWORD_DOUBLE_Q,
+    KEYWORD_ELSE,
+    KEYWORD_FUNC,
+    KEYWORD_IF,
+    KEYWORD_INT,
+    KEYWORD_INT_Q,
+    KEYWORD_LET,
+    KEYWORD_NIL,
+    KEYWORD_RETURN,
+    KEYWORD_STRING,
+    KEYWORD_STRING_Q,
+    KEYWORD_VAR,
+    KEYWORD_WHILE,
 } type_of_token;
 
 typedef union Value_of_token
@@ -108,19 +122,19 @@ typedef union Value_of_token
     char *STR_VAL;
 } value_of_token;
 
-typedef struct Dynamic_String
-{
-    char *data;            // current cuntent
-    unsigned int size;     // current size
-    unsigned int capacity; // max capacity
-} dynamic_string;
-
 typedef struct Token
 {
     value_of_token token_value;
     type_of_token token_type;
     int line;
 } lex_token;
+
+typedef struct Dynamic_String
+{
+    char *data;            // current cuntent
+    unsigned int size;     // current size
+    unsigned int capacity; // max capacity
+} dynamic_string;
 
 // function to get new token
 lex_token get_next_token();
