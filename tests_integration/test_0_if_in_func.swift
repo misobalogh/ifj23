f50707dea472f06705855157752dcdd74760640a
Michal Balogh <baloghmichal03@gmail.com>
2023-12-02 17:59:46 +0100

    if return in func fix

---
+++ tests_integration/test_0_if_in_func.swift
@@ -0,0 +1,21 @@
+func foo() -> Int {
+    if 1 == 1 {
+        return 2
+    }
+    else {
+        return 3
+    }
+    let a : Int = 3
+    if let a {
+        return 1
+    }
+    else {
+        return 0
+    }
+
+    while 3>4 {
+        return 4
+    }
+    
+}
+
